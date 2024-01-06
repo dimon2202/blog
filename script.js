3a893944546b813253ca86e110a3523cf316724f
Dima Sidenko <sidenkodima2202@gmail.com>
2024-01-06 10:31:48 +0200

    update info

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-
   function getCurrentDate() {
     const options = {
       weekday: "long",
@@ -15,78 +14,44 @@ document.addEventListener("DOMContentLoaded", function () {
   const loading = document.getElementById("loading");
   const slider = document.getElementById("slider");
   const userList = document.getElementById("userList");
-  const prevButton = document.getElementById('prevButton');
-  const nextButton = document.getElementById('nextButton');
+  const prevButton = document.getElementById("prevButton");
+  const nextButton = document.getElementById("nextButton");
 
-  // Show loading spinner while fetching posts data
   loading.style.display = "flex";
 
   // Fetch posts data
-//   fetch("https://jsonplaceholder.typicode.com/posts")
-//     .then((response) => response.json())
-//     .then((posts) => {
-//       // Hide loading spinner after data is loaded
-//       loading.style.display = "none";
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((posts) => {
+      loading.style.display = "none";
 
-//       posts.slice(0, 10).forEach((post) => {
-//         const postElement = document.createElement("div");
-//         postElement.classList.add("post");
-//         postElement.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
-//         slider.appendChild(postElement);
-//       });
-//     });
+      const sliderWrapper = document.querySelector(".swiper-wrapper");
 
+      posts.slice(0, 10).forEach((post) => {
+        const postElement = document.createElement("div");
+        postElement.classList.add("swiper-slide");
+        postElement.innerHTML = `<div><h3>${post.title}</h3><p>${post.body}</p></div>`;
+        sliderWrapper.appendChild(postElement);
+      });
 
-
-
-    // Fetch posts data
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(posts => {
-        // Hide loading spinner after data is loaded
-        loading.style.display = 'none';
-
-        const sliderWrapper = document.querySelector('.swiper-wrapper');
-
-        posts.slice(0, 10).forEach(post => {
-            const postElement = document.createElement('div');
-            postElement.classList.add('swiper-slide');
-            postElement.innerHTML = `<div><h3>${post.title}</h3><p>${post.body}</p></div>`;
-            sliderWrapper.appendChild(postElement);
-        });
-
-        // Initialize Swiper
-        const swiper = new Swiper('.swiper-container', {
-            slidesPerView: 3,
-            spaceBetween: 10,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-        });
-
-        });
-
-
-
-
-
-
-
-
-
-
-
+      const swiper = new Swiper(".swiper-container", {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    });
 
   // Fetch users data
   fetch("https://jsonplaceholder.typicode.com/users")
     .then((response) => response.json())
     .then((users) => {
-      // Hide loading spinner after data is loaded
       loading.style.display = "none";
 
       users.forEach((user) => {
@@ -100,33 +65,9 @@ document.addEventListener("DOMContentLoaded", function () {
           });
 
         userElement.addEventListener("click", () => {
-          // Redirect to user's blog page
           window.location.href = `user.html?id=${user.id}`;
         });
         userList.appendChild(userElement);
       });
     });
-
-    //  // Slider navigation
-    //  let currentIndex = 0;
-
-    //  function updateSlider() {
-    //      const scrollPosition = currentIndex * 302; // Considering 1rem margin
-    //      slider.scrollTo({ left: scrollPosition, behavior: 'smooth' });
-    //  }
-
-    //  prevButton.addEventListener('click', () => {
-    //      if (currentIndex > 0) {
-    //          currentIndex--;
-    //          updateSlider();
-    //      }
-    //  });
-
-    //  nextButton.addEventListener('click', () => {
-    //      const maxIndex = slider.children.length - 1;
-    //      if (currentIndex < maxIndex) {
-    //          currentIndex++;
-    //          updateSlider();
-    //      }
-    //  });
 });
